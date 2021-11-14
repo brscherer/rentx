2d57862b550a7c8d4ba86b6a7122872a826c7ced
Bruno Scherer <bruno.raphael96@gmail.com>
2021-11-14 11:02:49 -0300

    feat: add styled-components

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import {
+  Container,
+  Title 
+} from "./styles";
+
+export function Home(){
+  return (
+    <Container>
+      <Title>
+        Home
+      </Title>
+    </Container>
+  )
+}
