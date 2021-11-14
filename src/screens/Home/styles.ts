bf4c64c5b9532aee506d37267c4883d77808bc1e
Bruno Scherer <bruno.raphael96@gmail.com>
2021-11-14 11:27:25 -0300

    chore: add global styles

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,6 +4,10 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  background-color: ${({ theme }) => theme.colors.background_primary};
 `
 
-export const Title = styled.Text``
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  font-size: 30px;
+`
